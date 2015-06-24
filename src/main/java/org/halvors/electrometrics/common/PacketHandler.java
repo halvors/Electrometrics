6ec557b157472dee12055774906c372fd8dba2f7
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-06-24 11:10:25 +0200

    Added getPlayer() function to PacketHandler.

--- src/main/java/org/halvors/electrometrics/common/PacketHandler.java
+++ src/main/java/org/halvors/electrometrics/common/PacketHandler.java
@@ -1,8 +1,11 @@
 package org.halvors.electrometrics.common;
 
 import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 import org.halvors.electrometrics.Reference;
 import org.halvors.electrometrics.common.network.ElectricityMeterMessage;
 
@@ -11,5 +14,14 @@ public class PacketHandler {
 
     public void initialize() {
         network.registerMessage(ElectricityMeterMessage.Handler.class, ElectricityMeterMessage.class, 0, Side.CLIENT);
+        network.registerMessage(ElectricityMeterMessage.Handler.class, ElectricityMeterMessage.class, 0, Side.SERVER);
+    }
+
+    public static EntityPlayer getPlayer(MessageContext context) {
+        return context.side.isClient() ? Minecraft.getMinecraft().thePlayer : context.getServerHandler().playerEntity;
+    }
+
+    public static SimpleNetworkWrapper getNetwork() {
+        return network;
     }
 }
