46779c3288b70abb1b49e78443b6fc65b2b9d17b
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-06-25 14:39:56 +0200

    Added GuiScreen name.

--- src/main/java/org/halvors/electrometrics/client/gui/GuiScreen.java
+++ src/main/java/org/halvors/electrometrics/client/gui/GuiScreen.java
@@ -1,17 +1,24 @@
 package org.halvors.electrometrics.client.gui;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.util.ResourceLocation;
 import org.halvors.electrometrics.Reference;
 import org.lwjgl.opengl.GL11;
 
+@SideOnly(Side.CLIENT)
 public class GuiScreen extends net.minecraft.client.gui.GuiScreen {
     private static final ResourceLocation texture = new ResourceLocation(Reference.DOMAIN, "gui/guiBlank.png");
 
+    // The name of this GuiScreen.
+    private String name;
+
+    // The size dimensions of this GuiScreen.
     protected int xSize = 176;
     protected int ySize = 166;
 
-    public GuiScreen() {
-
+    public GuiScreen(String name) {
+        this.name = name;
     }
 
     @Override
@@ -21,14 +28,21 @@ public class GuiScreen extends net.minecraft.client.gui.GuiScreen {
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTick) {
-        int guiWidth = (width - xSize) / 2;
-        int guiHeight = (height - ySize) / 2;
-
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         mc.renderEngine.bindTexture(texture);
 
+        int guiWidth = (width - xSize) / 2;
+        int guiHeight = (height - ySize) / 2;
+
         drawTexturedModalRect(guiWidth, guiHeight, 0, 0, xSize, ySize);
 
+        // Display the name of this GuiScreen.
+        fontRendererObj.drawString(getName(), guiWidth + (xSize / 2), guiHeight + 5, 0x404040);
+
         super.drawScreen(mouseX, mouseY, partialTick);
+    }
+
+    public String getName() {
+        return name;
     }
 }
