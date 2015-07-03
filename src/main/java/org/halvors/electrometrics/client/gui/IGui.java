095f9446e663aef65f88fb13b3672ca33b842099
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-07-03 08:25:25 +0200

    Documented IGui.

--- src/main/java/org/halvors/electrometrics/client/gui/IGui.java
+++ src/main/java/org/halvors/electrometrics/client/gui/IGui.java
@@ -9,14 +9,47 @@ import java.util.List;
 
 @SideOnly(Side.CLIENT)
 public interface IGui {
+    /**
+     * Draws a textured rectangle in this GUI.
+     * @param x
+     * @param y
+     * @param u
+     * @param v
+     * @param w
+     * @param h
+     */
 	void drawTexturedRect(int x, int y, int u, int v, int w, int h);
 
+    /**
+     * Draws a textured rectangle from the specified icon.
+     * @param x
+     * @param y
+     * @param icon
+     * @param w
+     * @param h
+     */
 	void drawTexturedRectFromIcon(int x, int y, IIcon icon, int w, int h);
 
+    /**
+     * Display the specified string as tooltip at the specified location.
+     * @param s
+     * @param xAxis
+     * @param yAxis
+     */
 	void displayTooltip(String s, int xAxis, int yAxis);
 
+    /**
+     * Display a list of tooltips at the specified location.
+     * @param list
+     * @param xAxis
+     * @param yAxis
+     */
 	void displayTooltips(List<String> list, int xAxis, int yAxis);
 
+    /**
+     * The fontrenderer object of this GUI.
+     * @return FontRenderer
+     */
 	FontRenderer getFontRenderer();
 }
 
