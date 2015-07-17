88bd23e3f68c6fc81fc2a049615d4e0e8534e7bb
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-07-17 15:04:06 +0200

    Getting the correct TileEntity for GuiMachine.

--- src/main/java/org/halvors/electrometrics/client/gui/GuiMachine.java
+++ src/main/java/org/halvors/electrometrics/client/gui/GuiMachine.java
@@ -2,16 +2,11 @@ package org.halvors.electrometrics.client.gui;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import org.halvors.electrometrics.common.tile.TileEntityRotatable;
+import org.halvors.electrometrics.common.tile.TileEntityMachine;
 
 @SideOnly(Side.CLIENT)
 public class GuiMachine extends GuiComponentScreen {
-	GuiMachine(TileEntityRotatable tileEntity) {
+	GuiMachine(TileEntityMachine tileEntity) {
 		super(tileEntity);
-	}
-
-	@Override
-	void drawGuiScreenForegroundLayer(int mouseX, int mouseY) {
-		fontRendererObj.drawString(tileEntity.getName(), (xSize / 2) - (fontRendererObj.getStringWidth(tileEntity.getName()) / 2), 6, 0x404040);
 	}
 }
