0d904046298c91d327c50a9c6572438db795a68b
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-07-20 07:24:09 +0200

    Fixed so that Key functions is only available client side.

--- src/main/java/org/halvors/electrometrics/client/key/Key.java
+++ src/main/java/org/halvors/electrometrics/client/key/Key.java
@@ -1,9 +1,12 @@
 package org.halvors.electrometrics.client.key;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.client.settings.KeyBinding;
 
+@SideOnly(Side.CLIENT)
 public class Key {
     private static final GameSettings gameSettings = Minecraft.getMinecraft().gameSettings;
 
--- src/main/java/org/halvors/electrometrics/client/key/KeyHandler.java
+++ src/main/java/org/halvors/electrometrics/client/key/KeyHandler.java
@@ -1,9 +1,12 @@
 package org.halvors.electrometrics.client.key;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.settings.KeyBinding;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 
+@SideOnly(Side.CLIENT)
 public class KeyHandler {
     public static boolean getIsKeyPressed(KeyBinding keyBinding) {
         int keyCode = keyBinding.getKeyCode();
