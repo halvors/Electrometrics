1ff5c8c59febbfeac571a967928ed20bb0523b6e
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-07-22 08:29:27 +0200

    Fixed crash bug caused by unintentional override.

--- src/main/java/org/halvors/electrometrics/common/network/PacketRequestData.java
+++ src/main/java/org/halvors/electrometrics/common/network/PacketRequestData.java
@@ -3,7 +3,6 @@ package org.halvors.electrometrics.common.network;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
-import io.netty.buffer.ByteBuf;
 import org.halvors.electrometrics.common.base.tile.ITileNetworkable;
 import org.halvors.electrometrics.common.tile.TileEntity;
 import org.halvors.electrometrics.common.tile.component.ITileComponent;
@@ -24,16 +23,6 @@ public class PacketRequestData extends PacketBlockLocation implements IMessage {
 
 	public PacketRequestData(ITileComponent tileComponent) {
 		this(tileComponent.getTileEntity());
-	}
-
-	@Override
-	public void fromBytes(ByteBuf dataStream) {
-
-	}
-
-	@Override
-	public void toBytes(ByteBuf dataStream) {
-
 	}
 
 	public static class PacketRequestDataMessage implements IMessageHandler<PacketRequestData, IMessage> {
