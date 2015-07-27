ea7740359b9ced017ccea57f2ca127a23ca0e1c0
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-07-27 12:27:13 +0200

    Added method for getting the netive minecraft TileEntity for various purposes.

--- src/main/java/org/halvors/electrometrics/common/tile/TileEntity.java
+++ src/main/java/org/halvors/electrometrics/common/tile/TileEntity.java
@@ -5,12 +5,16 @@ import net.minecraft.world.IBlockAccess;
 public class TileEntity extends net.minecraft.tileentity.TileEntity {
 	private final String inventoryName;
 
-	TileEntity(String inventoryName) {
+	protected TileEntity(String inventoryName) {
 		this.inventoryName = inventoryName;
 	}
 
 	public String getInventoryName() {
 		return inventoryName;
+	}
+
+	public net.minecraft.tileentity.TileEntity getNative() {
+		return this;
 	}
 
 	public static TileEntity getTileEntity(IBlockAccess world, int x, int y, int z) {
